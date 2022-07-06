7b3d8eb20623cc63a44fea11411b7ad18f8aa6d9
DuranUnverdi <75857104+DuranUnverdi@users.noreply.github.com>
2022-07-06 15:54:01 +0300

    Parameterizing the Test data with the data taken from Json File into tests

---
+++ CSharpSelFramework/Utilities/jsonreader.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CSharpSelFramework.Utilities
+{
+    public class jsonreader
+    {
+        public jsonreader()
+        {
+        }
+        public void extractData()
+        {
+            //json dosyasını ulaştık
+           var myJsonString= File.ReadAllText("Utilities/testData.json");
+            //jsonu parse ettik
+           var jsonObject= JToken.Parse(myJsonString);
+            //username string şekilde ekrana bastık 
+           Console.WriteLine(jsonObject.SelectToken("username").Value<string>());
+        }
+    }
+}
